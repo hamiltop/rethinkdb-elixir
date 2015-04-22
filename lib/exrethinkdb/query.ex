25bea28e0f3d472b1aff7d85c68f0c4920de2f46
Manuel Kallenbach <mk@manukall.de>
2015-04-22 14:55:40 +0200

    create, list and delete databases and tables

--- lib/exrethinkdb/query.ex
+++ lib/exrethinkdb/query.ex
@@ -3,6 +3,21 @@ defmodule Exrethinkdb.Query do
   def table(name), do:        [15, [name]]
   def table(query, name), do: [15, [query, name]]
 
+  def db_create(name), do:    [57, [name]]
+  def db_drop(name), do:      [58, [name]]
+  def db_list, do:            [59]
+
+  def table_create(db_query, name, options), do: [60, [db_query, name], options]
+  def table_create(name, options = %{}), do: [60, [name], options]
+  def table_create(db_query, name), do: [60, [db_query, name]]
+  def table_create(name), do: [60, [name]]
+
+  def table_drop(db_query, name), do:   [61, [db_query, name]]
+  def table_drop(name), do:   [61, [name]]
+
+  def table_list(db_query), do: [62, [db_query]]
+  def table_list, do: [62]
+
   def filter(query, filter), do: [39, [query, filter]]
 
   def get(query, id), do: [16, [query,  id]]
