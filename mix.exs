4901ec794e3c2b47958ffabb3c4db89151b0287c
Peter Hamilton <peterghamilton@gmail.com>
2015-08-30 12:39:26 -0700

    Bump to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RethinkDB.Mixfile do
 
   def project do
     [app: :rethinkdb,
-     version: "0.0.5",
+     version: "0.1.0",
      elixir: "~> 1.0",
      description: "RethinkDB driver for Elixir",
      package: package,
