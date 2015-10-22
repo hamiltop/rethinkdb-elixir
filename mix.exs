8e6821c3ea1794bd0d108f496bac7251883e47bd
Peter Hamilton <peterghamilton@gmail.com>
2015-10-21 22:57:47 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RethinkDB.Mixfile do use Mix.Project
 
   def project do
     [app: :rethinkdb,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.0",
      description: "RethinkDB driver for Elixir",
      package: package,
