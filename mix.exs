c8c2155e27eb31c9e1346c671382998aaae28c3b
Peter Hamilton <peterghamilton@gmail.com>
2015-11-28 15:31:22 -0800

    Update hex version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RethinkDB.Mixfile do use Mix.Project
 
   def project do
     [app: :rethinkdb,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      description: "RethinkDB driver for Elixir",
      package: package,
